e187130c34bae0d58fe68048a787767bd55c2f58
Anastasiia <anastasya.zolotova11@gmail.com>
2023-12-11 12:33:57 +0000

    Authentication improvement v0.1

--- app/src/main/java/com/example/restaurantreviewapp/MainActivity.kt
+++ app/src/main/java/com/example/restaurantreviewapp/MainActivity.kt
@@ -1,16 +1,9 @@
 package com.example.restaurantreviewapp
 
-import android.content.Context
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.fragment.app.Fragment
 import com.example.restaurantreviewapp.databinding.ActivityMainBinding
-import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
@@ -22,10 +15,16 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
     }
 
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
 }
